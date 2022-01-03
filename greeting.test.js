cd19f61e0f3e69b50ca838476e3fdc9e69d1c162
heyameen <aladeameen@gmail.com>
2022-01-03 16:19:03 +0100

    :truck: seperate tests into a specific folder

--- greeting.test.js
+++
@@ -1,7 +0,0 @@
-const greeting = guest => `Hello, ${guest}!`;
-
-describe('greeting()', () => { // 1
-  it('says hello', () => { // 2
-    expect(greeting('Jest')).toBe('Hello, Jest!'); // 3
-  });
-});
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,4 @@
-const fizzBuzz = require('./index');
+const fizzBuzz = require('../fizzBuzz/index');
 
 describe('fizzBuzz', () => {
     it('returns "FizBuzz" for multiples of 3 and 5', () => {
@@ -11,12 +11,12 @@ describe('fizzBuzz', () => {
         expect(fizzBuzz(22)).toBe('22')
     })
     
-    it('returns "Buzz" for multiples of 5 ', () => {
+    it('returns "Buzz" for multiples of 5', () => {
         expect(fizzBuzz(10)).toBe('Buzz')
         expect(fizzBuzz(20)).toBe('Buzz')
     })
     
-    it('returns "Fizz" for multiples of 3 ', () => {
+    it('returns "Fizz" for multiples of 3', () => {
         expect(fizzBuzz(9)).toBe('Fizz')
         expect(fizzBuzz(12)).toBe('Fizz')
     })
