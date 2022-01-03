6c2f49439ac6a5e664dd16cb53e2f57ca70853c7
heyameen <aladeameen@gmail.com>
2022-01-03 15:13:37 +0100

    :tada: First commit

---
+++ index.test.js
@@ -0,0 +1,27 @@
+const fizzBuzz = require('./index');
+
+describe('fizzBuzz', () => {
+    it('returns "FizBuzz" for multiples of 3 and 5', () => {
+        expect(fizzBuzz(15)).toBe('FizzBuzz')
+        expect(fizzBuzz(30)).toBe('FizzBuzz')
+    })
+
+    it('returns the given number for multiplies of neither 3 nor 5', () => {
+        expect(fizzBuzz(1)).toBe('1')
+        expect(fizzBuzz(22)).toBe('22')
+    })
+    
+    it('returns "Buzz" for multiples of 5 ', () => {
+        expect(fizzBuzz(10)).toBe('Buzz')
+        expect(fizzBuzz(20)).toBe('Buzz')
+    })
+    
+    it('returns "Fizz" for multiples of 3 ', () => {
+        expect(fizzBuzz(9)).toBe('Fizz')
+        expect(fizzBuzz(12)).toBe('Fizz')
+    })
+
+    
+
+    
+})
